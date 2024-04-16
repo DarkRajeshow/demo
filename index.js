14a55d65eeb86e961acf80157193ce1b40ebab16
Rajesh Adeli <114233607+DarkRajeshow@users.noreply.github.com>
2024-04-16 21:58:22 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function mouseoutimg() {
+    document.querySelector(".col-lg-4").style.transition = "0.5s"
+    document.querySelector(".col-lg-4").nextElementSibling.style.transition = "0.5s"
+    document.querySelector("#flex>div>img").style.transition = "0.5s"
+    document.querySelector(".row3").lastElementChild.style.transition = "0.5s"
+    document.querySelector(".img1>img").style.transition = "1s"
+    document.querySelectorAll(".mans")[0].style.transition = "0.5s"
+    document.querySelectorAll(".mans")[1].style.transition = "0.5s"
+}
